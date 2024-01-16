fe87e207aaf89b171fb2d7e359fa43712a564211
lucasabdouni <lucasabdouni@hotmail.com>
2024-01-16 16:26:06 -0300

    feat: create todo list type and functions delete

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrl: './todo-list.component.scss'
 })
 export class TodoListComponent {
+  public taskList: Array<TaskList> = [
+    {
+      task: "Minha nova Task",
+      checked: true
+    },
+    {
+      task: "Minha nova Task 2",
+      checked: false
+    }
+  ];
 
+  constructor() {}
+
+  public deleteItemTaskList(event: number){
+    this.taskList.splice(event, 1)
+  }
+
+  public deleteAllTaskList(){
+    const confirm = window.confirm("Você deseja realmente Deletar tudo?")
+
+    if (confirm) {
+      this.taskList = []
+    }
+  }
 }
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -9,8 +9,7 @@ import { TodoListComponent } from './components/todo-list/todo-list.component';
 
 //Pages
 import { HomeComponent } from './pages/home/home.component';
-
-
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -21,7 +20,8 @@ import { HomeComponent } from './pages/home/home.component';
     HomeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ]
 })
 export class HomeModule { }
