103927669c1bd8d97cef124f99bdd1778f04352c
lucasabdouni <lucasabdouni@hotmail.com>
2024-01-16 16:48:55 -0300

    feat: created function to add list and emit event through output

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -7,18 +7,13 @@ import { TaskList } from '../../model/task-list';
   styleUrl: './todo-list.component.scss'
 })
 export class TodoListComponent {
-  public taskList: Array<TaskList> = [
-    {
-      task: "Minha nova Task",
-      checked: true
-    },
-    {
-      task: "Minha nova Task 2",
-      checked: false
-    }
-  ];
+  public taskList: Array<TaskList> = [];
 
   constructor() {}
+
+  public setEmmitTaskList(event: string) {
+    this.taskList.push({task: event, checked: false})
+  }
 
   public deleteItemTaskList(event: number){
     this.taskList.splice(event, 1)
